215caa08b3df2304beff3d99247de9374fae9df8
Quinten Konyn <qkonyn@gmail.com>
2023-11-23 23:37:07 -0800

    Generate image and write to disk
    Following the first task of [this tutorial](https://github.com/ssloy/tinyraycaster/wiki/Part-1:-crude-3D-renderings#step-1-save-an-image-to-disk)
    but haven't yet looked at their code to decide if I want to copy over
    any techniques.
    
    Still need to handle file io errors. Currently fail silently.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use image::Rgb;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let w = 512;
+    let h = 512;
+    let mut img = image::RgbImage::new(w, h);
+
+    for y in 0..h {
+        for x in 0..w {
+            let r = ((255 * y) as f32 / (w as f32)) as u8;
+            let g = ((255 * x) as f32 / (w as f32)) as u8;
+            let b = 0;
+            img.put_pixel(x, y, Rgb([r, g, b]));
+        }
+    }
+
+    // make the output directory if it doesn't already exist
+    let _ = fs::create_dir("output");
+    // write the image
+    let _ = image::save_buffer("output/render.png", &img, w, h, image::ColorType::Rgb8);
 }
