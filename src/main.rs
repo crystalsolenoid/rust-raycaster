5113b53bc6fd1d20f1c8ff75de835dec79be323a
Quinten Konyn <qkonyn@gmail.com>
2023-11-24 14:41:05 -0800

    Generate outer walls

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,55 @@ fn write_image(img: &image::RgbImage) {
     let _ = image::save_buffer("output/render.png", img, dims.0, dims.1, kind);
 }
 
+fn gen_map(w: u32, h: u32) -> Vec<bool> {
+    let mut map = vec![false; (w * h) as usize];
+
+    let mut draw_rect = |x1: u32, y1: u32, x2: u32, y2: u32| {
+        // I don't know how a closure works,
+        // but the compiler recommended it
+        // so here we go!
+        for y in y1..y2 {
+            for x in x1..x2 {
+                let idx = (x + y * w) as usize;
+                map[idx] = true;
+            }
+        }
+    };
+
+    // test map
+    const THICKNESS: u32 = 32;
+    // outer walls
+    draw_rect(0, 0, THICKNESS, h);
+    draw_rect(0, 0, w, THICKNESS);
+    draw_rect(w - THICKNESS, 0, w, h);
+    draw_rect(0, h - THICKNESS, w, h);
+
+    map
+}
+
 fn main() {
-    let w = 512;
-    let h = 512;
+    let w: u32 = 512;
+    let h: u32 = 512;
     let mut img = image::RgbImage::new(w, h);
+
+    let map = gen_map(w, h);
 
     for y in 0..h {
         for x in 0..w {
-            let r = ((255 * y) as f32 / (w as f32)) as u8;
-            let g = ((255 * x) as f32 / (w as f32)) as u8;
-            let b = 0;
+            let idx = (x + y * w) as usize;
+            let wall = map[idx];
+            let r;
+            let g;
+            let b;
+            if wall {
+                r = 0 as u8;
+                g = 128 as u8;
+                b = 200 as u8;
+            } else {
+                r = ((255 * y) as f32 / (w as f32)) as u8;
+                g = ((255 * x) as f32 / (w as f32)) as u8;
+                b = 0;
+            }
             img.put_pixel(x, y, Rgb([r, g, b]));
         }
     }
