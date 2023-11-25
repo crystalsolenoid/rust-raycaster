fdf1743194bb0346a368cf059e979be272f37d21
Quinten Konyn <qkonyn@gmail.com>
2023-11-24 19:27:57 -0800

    Allow different wall materials

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,15 @@ struct Camera {
     fov: f32,
 }
 
+#[derive(Clone, Copy)]
+enum Wall {
+    Dirt,
+    Brick,
+    Stone,
+}
+
 fn cast_fov(w: u32, img: &mut image::RgbImage,
-            map: &[bool], cam: &Camera) {
+            map: &[Option<Wall>], cam: &Camera) {
     for i in 0..512 {
         let step = (i as f32) / 512.0;
         cast_ray(w, img, &map, &cam, step);
@@ -19,7 +26,7 @@ fn cast_fov(w: u32, img: &mut image::RgbImage,
 
 fn cast_ray(w: u32,
             img: &mut image::RgbImage,
-            map: &[bool], cam: &Camera, span: f32) {
+            map: &[Option<Wall>], cam: &Camera, span: f32) -> Option<Wall> {
     // step ranges from 0 to 1: percentage throug the fov
     let angle = cam.radians + cam.fov * (span - 0.5);
     const STEPS: u32 = 100;
@@ -31,12 +38,14 @@ fn cast_ray(w: u32,
         let x = cam.x + x_off;
         let y = cam.y - y_off; // minus because +y is down
         let idx = (x + y * w) as usize;
-        if map[idx] {
-            break;
+        // TODO: make this pattern matching
+        if map[idx].is_some() {
+            return map[idx];
         } else {
             img.put_pixel(x, y, Rgb([86, 50, 38]));
         }
     }
+    return None;
 }
 
 fn draw_camera(img: &mut image::RgbImage, camera: &Camera) {
@@ -59,53 +68,59 @@ fn write_image(img: &image::RgbImage) {
     let _ = image::save_buffer("output/render.png", img, dims.0, dims.1, kind);
 }
 
-fn gen_map(w: u32, h: u32) -> Vec<bool> {
-    let mut map = vec![false; (w * h) as usize];
+fn gen_map(w: u32, h: u32) -> Vec<Option<Wall>> {
+//    let mut map = vec![None; (w * h) as usize];
+    let mut map = Vec::with_capacity((w * h) as usize);
+    map.resize((w * h) as usize, None);
 
-    let draw_rect = |map: &mut [bool],
-                     x1: u32, y1: u32, x2: u32, y2: u32| {
+    let draw_rect = |map: &mut [Option<Wall>],
+                     x1: u32, y1: u32, x2: u32, y2: u32, material: Option<Wall>| {
         for y in y1..y2 {
             for x in x1..x2 {
                 let idx = (x + y * w) as usize;
-                map[idx] = true;
+                map[idx] = material;
             }
         }
     };
 
-    let horiz_wall = |map: &mut [bool],
-                      x1: u32, x2: u32, y1: u32| {
-        draw_rect(map, x1, y1, x2, y1 + THICKNESS);
+    let horiz_wall = |map: &mut [Option<Wall>],
+                      x1: u32, x2: u32, y1: u32, material: Option<Wall>| {
+        draw_rect(map, x1, y1, x2, y1 + THICKNESS, material);
     };
 
-    let vert_wall = |map: &mut [bool],
-                      y1: u32, y2: u32, x1: u32| {
-        draw_rect(map, x1, y1, x1 + THICKNESS, y2);
+    let vert_wall = |map: &mut [Option<Wall>],
+                      y1: u32, y2: u32, x1: u32, material: Option<Wall>| {
+        draw_rect(map, x1, y1, x1 + THICKNESS, y2, material);
     };
 
     // test map
     const THICKNESS: u32 = 32;
     // outer walls
-    draw_rect(&mut map, 0, 0, THICKNESS, h);
-    draw_rect(&mut map, 0, 0, w, THICKNESS);
-    draw_rect(&mut map, w - THICKNESS, 0, w, h);
-    draw_rect(&mut map, 0, h - THICKNESS, w, h);
+    let mut material = Some(Wall::Dirt);
+    draw_rect(&mut map, 0, 0, THICKNESS, h, material);
+    draw_rect(&mut map, 0, 0, w, THICKNESS, material);
+    draw_rect(&mut map, w - THICKNESS, 0, w, h, material);
+    draw_rect(&mut map, 0, h - THICKNESS, w, h, material);
     // inner walls
     // little room
-    horiz_wall(&mut map, 0, 150, 200);
-    horiz_wall(&mut map, 0, 150, 400);
-    vert_wall(&mut map, 200, 280, 150);
-    vert_wall(&mut map, 320, 400 + THICKNESS, 150);
+    material = Some(Wall::Stone);
+    horiz_wall(&mut map, 0, 150, 200, material);
+    horiz_wall(&mut map, 0, 150, 400, material);
+    vert_wall(&mut map, 200, 280, 150, material);
+    vert_wall(&mut map, 320, 400 + THICKNESS, 150, material);
     // hallway
-    vert_wall(&mut map, 100, h, 250);
-    horiz_wall(&mut map, 100, 250, 100);
-    horiz_wall(&mut map, 340, w, 100);
-    horiz_wall(&mut map, 250 + THICKNESS, 450, 170);
+    material = Some(Wall::Brick);
+    vert_wall(&mut map, 100, h, 250, material);
+    horiz_wall(&mut map, 100, 250, 100, material);
+    horiz_wall(&mut map, 340, w, 100, material);
+    horiz_wall(&mut map, 250 + THICKNESS, 450, 170, material);
     // bumps
-    vert_wall(&mut map, 450, h, 400);
-    vert_wall(&mut map, 450, h, 350);
-    vert_wall(&mut map, 450, h, 300);
+    material = Some(Wall::Brick);
+    vert_wall(&mut map, 450, h, 400, material);
+    vert_wall(&mut map, 450, h, 350, material);
+    vert_wall(&mut map, 450, h, 300, material);
     // columns
-    vert_wall(&mut map, 300, 300 + THICKNESS, 380);
+    vert_wall(&mut map, 300, 300 + THICKNESS, 380, material);
 
     map
 }
@@ -130,14 +145,27 @@ fn main() {
             let r;
             let g;
             let b;
-            if wall {
-                r = 114;
-                g = 89;
-                b = 86;
-            } else {
-                r = 32;
-                g = 32;
-                b = 32;
+            match wall {
+                Some(Wall::Dirt) => {
+                    r = 86;
+                    g = 50;
+                    b = 38;
+                },
+                Some(Wall::Brick) => {
+                    r = 246;
+                    g = 205;
+                    b = 38;
+                },
+                Some(Wall::Stone) => {
+                    r = 57;
+                    g = 57;
+                    b = 57;
+                },
+                _ => {
+                    r = 32;
+                    g = 32;
+                    b = 32;
+                }
             }
             img.put_pixel(x, y, Rgb([r, g, b]));
         }
